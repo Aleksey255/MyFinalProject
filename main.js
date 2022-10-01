36b6b4fe470cd48a5fdfa4e4010823d62b32299e
Aleksey255 <aleksey-255@mail.ru>
2022-10-01 21:38:52 +0500

    3 blok

--- main.js
+++ main.js
@@ -12,9 +12,7 @@ fetch("https://api.openweathermap.org/data/2.5/weather?q=москва&lang=ru&ap
     })
 
 let inp = document.querySelector(".ad")
-let inp1 = document.querySelector(".del")
 let butt = document.querySelector(".btn")
-let butt1 = document.querySelector(".btn2")
 let ul = document.querySelector('.ul');
 butt.onclick = () => {
     let li = document.createElement('li');
@@ -23,6 +21,7 @@ butt.onclick = () => {
     let btn = document.createElement("button")
     li.append(btn)
     btn.textContent = "Удалить"
+    btn.style.marginLeft = "3em"
 }
 ul.addEventListener('click', function (event) {
     if (event.target.nodeName === 'BUTTON') {
@@ -30,3 +29,81 @@ ul.addEventListener('click', function (event) {
         event.target.closest('li').remove()
     }
 })
+
+let out = document.querySelector(".result")
+let x = ""
+let y = ""
+let oper = ""
+finish = false
+
+let number = document.querySelectorAll(".num")
+console.log(number);
+for (let i = 0; i < number.length; i++) {
+    number[i].onclick = function () {
+        let num = this.getAttribute("data")
+        if (y == "" && oper == "") {
+            x += num
+            out.textContent = x
+            console.log(x, oper, y);
+        }
+        else if (x !== "" && y !== "" && finish) {
+            y = num
+            finish = false
+            out.textContent = y
+        }
+        else {
+            y += num
+            out.textContent = y
+        }
+        console.log(x, oper, y);
+    }
+}
+
+let operation = document.querySelectorAll(".op")
+console.log(operation);
+for (let j = 0; j < operation.length; j++) {
+    operation[j].onclick = function () {
+        num = this.getAttribute("data")
+        oper = num
+        out.textContent = oper
+        console.log(x, oper, y);
+    }
+
+}
+
+document.querySelector(".ravno").onclick = function () {
+    if (y == "") y = x
+    switch (oper) {
+        case "+":
+            x = (+x) + (+y)
+            break;
+        case "-":
+            x = x - y
+            break
+        case "*":
+            x = x * y
+            break
+        case "/":
+            if (y == "0") {
+                out.textContent = "Ошибка"
+                x = ""
+                y = ""
+                oper = ""
+            }
+            x = x / y
+            break
+    }
+    finish = true
+    out.textContent = x
+    console.log(x, oper, y);
+}
+
+document.querySelector(".backspace").onclick = function () {
+    out.textContent = 0
+    x = ""
+    y = ""
+    oper = ""
+}
+
+
+
